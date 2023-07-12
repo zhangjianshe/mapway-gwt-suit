305cbccdcaa57b948955bc0f36cef1c083be9886
zhangjianshe <zhangjianshe@gmail.com>
2023-07-12 15:05:02 +0800

    添加多文件选择

--- mapway-gwt-common/src/main/java/cn/mapway/ui/client/mvc/attribute/IAttribute.java
+++ mapway-gwt-common/src/main/java/cn/mapway/ui/client/mvc/attribute/IAttribute.java
@@ -14,23 +14,12 @@ public interface IAttribute {
     int DT_FLOAT = 2;
     int DT_DATETIME = 3;
     int DT_BOOLEAN = 4;
-    // input type list 0 TextBox 1 DropDownList 2 CheckBox 3 Color
-    int INPUT_TEXTBOX = 0;
-    int INPUT_DROPDOWNLIST = 1;
-    int INPUT_CHECKBOX = 2;
-    int INPUT_COLOR = 3;
-    int INPUT_PATH = 4; //路径选择器
-    int INPUT_FILE = 5; //文件选择器
-    int INPUT_SLIDER = 6; //Slider选择输入
-    int INPUT_OTHERS=99;
+
 
     String getName();
 
     String getAltName();
 
-    int INPUT_PATHALL = 7;//目录或者文件选择
-    int INPUT_TEXTAREA = 8;//文本域
-    int INPUT_MULTIFILE = 9;//多文件选择
     Object getValue();
 
     void setValue(Object value);
