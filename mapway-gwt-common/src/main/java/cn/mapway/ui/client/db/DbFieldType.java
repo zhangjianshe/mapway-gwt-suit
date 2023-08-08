f5d44bc403b50b94d155a4f400e9b041c5eef9a7
zhangjianshe <zhangjianshe@gmail.com>
2023-08-08 09:31:00 +0800

    change toolbar ui

--- mapway-gwt-common/src/main/java/cn/mapway/ui/client/db/DbFieldType.java
+++ mapway-gwt-common/src/main/java/cn/mapway/ui/client/db/DbFieldType.java
@@ -21,7 +21,7 @@ public enum DbFieldType {
     FLD_TYPE_GEOMETRY(5, "地理字段", Fonts.EARTH),
     FLD_TYPE_SERIAL(7, "序列", Fonts.SERIAL),
     FLD_TYPE_STRING_ARRAY(8, "字符串数组", Fonts.LIST),
-    FLD_TYPE_CLOB(8, "文本块", Fonts.FILES);
+    FLD_TYPE_CLOB(9, "文本块", Fonts.FILES);
     //TODO 添加新的图标样式
     int code;
     String name;
