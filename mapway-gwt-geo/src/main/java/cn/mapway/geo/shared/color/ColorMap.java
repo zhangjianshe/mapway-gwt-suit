8f5c99840317ad83446c4479b4a031a41630f1f6
zhangjianshe <zhangjianshe@gmail.com>
2023-08-21 16:55:17 +0800

    change colortable defination

--- mapway-gwt-geo/src/main/java/cn/mapway/geo/shared/color/ColorMap.java
+++ mapway-gwt-geo/src/main/java/cn/mapway/geo/shared/color/ColorMap.java
@@ -1,12 +1,14 @@
 package cn.mapway.geo.shared.color;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
+import lombok.Data;
 
 import java.io.Serializable;
 
 /**
  * 颜色映射表
  */
+@Data
 public class ColorMap implements Serializable, IsSerializable {
     double start;
     double end;
