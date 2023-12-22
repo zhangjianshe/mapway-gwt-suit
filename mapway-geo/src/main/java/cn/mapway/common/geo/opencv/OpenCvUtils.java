94d764fae2927c388e03a1319c42af54fb1d7005
baoshuai <baoshuaiZealot@163.com>
2023-12-22 09:20:17 +0800

    暂时去除opencv的引入

--- mapway-geo/src/main/java/cn/mapway/common/geo/opencv/OpenCvUtils.java
+++ mapway-geo/src/main/java/cn/mapway/common/geo/opencv/OpenCvUtils.java
@@ -1,7 +1,6 @@
 package cn.mapway.common.geo.opencv;
 
 import lombok.extern.slf4j.Slf4j;
-import org.opencv.core.Core;
 
 /**
  * @Author baoshuaiZealot@163.com  2023/12/19
@@ -10,13 +9,13 @@ import org.opencv.core.Core;
 public class OpenCvUtils {
 
     public static void init(){
-        try{
-            log.info("初始化OpenCV");
-            System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-            log.info("初始化OpenCV 成功");
-        }catch (Exception e){
-            log.info("初始化OpenCV 失败. 失败原因", e.getMessage());
-        }
+//        try{
+//            log.info("初始化OpenCV");
+//            System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+//            log.info("初始化OpenCV 成功");
+//        }catch (Exception e){
+//            log.info("初始化OpenCV 失败. 失败原因", e.getMessage());
+//        }
 
     }
 
