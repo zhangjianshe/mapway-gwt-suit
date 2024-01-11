081c86c35a3292e3007239031443bda5d5151835
zhangjianshe <zhangjianshe@gmail.com>
2024-01-11 14:46:38 +0800

    dropdown add unicode support

--- mapway-gwt-common/src/main/java/cn/mapway/ui/client/mvc/Rect.java
+++ mapway-gwt-common/src/main/java/cn/mapway/ui/client/mvc/Rect.java
@@ -1,13 +1,10 @@
 package cn.mapway.ui.client.mvc;
 
-import jsinterop.annotations.JsOverlay;
-import jsinterop.annotations.JsPackage;
-import jsinterop.annotations.JsType;
+import com.google.gwt.user.client.rpc.IsSerializable;
 
 import java.io.Serializable;
 
-@JsType(isNative = true, name = "Object", namespace = JsPackage.GLOBAL)
-public class Rect  {
+public class Rect implements Serializable, IsSerializable {
     public double x;
     public double y;
     public double width;
@@ -22,19 +19,19 @@ public class Rect  {
      * @return
      */
     @Override
-    @JsOverlay
+
     public final String toString() {
         return "{\"x\":" + x + ",\"y\":" + y + ",\"width\":" + width + ",\"height\":" + height + "}";
     }
 
-    @JsOverlay
+
     public final Rect offset(double dx, double dy) {
         this.x += dx;
         this.y += dy;
         return this;
     }
 
-    @JsOverlay
+
     public final Rect expand(double top, double right, double bottom, double left) {
         this.x -= left;
         this.y -= top;
@@ -43,27 +40,27 @@ public class Rect  {
         return this;
     }
 
-    @JsOverlay
+
     public final Size getCenter() {
         return new Size(x + width / 2, y + height / 2);
     }
 
-    @JsOverlay
+
     public final Rect clone() {
         return new Rect().set(x, y, width, height);
     }
 
-    @JsOverlay
+
     public final boolean contains(Size point) {
         return point.x >= x && point.x <= x + width && point.y >= y && point.y <= y + height;
     }
 
-    @JsOverlay
+
     public final boolean contains(double x, double y) {
         return contains(new Size(x, y));
     }
 
-    @JsOverlay
+
     public final Rect extend(double x, double y) {
         if (this.x > x) {
             this.x = x;
@@ -80,7 +77,7 @@ public class Rect  {
         return this;
     }
 
-    @JsOverlay
+
     public final Rect extend(Rect box) {
         if (this.x > box.x) {
             this.x = box.x;
@@ -97,7 +94,7 @@ public class Rect  {
         return this;
     }
 
-    @JsOverlay
+
     public final Rect shrink(double left, double top, double right, double bottom) {
         x = x + left;
         y = y + top;
@@ -109,7 +106,7 @@ public class Rect  {
     /**
      * 正交化对象
      */
-    @JsOverlay
+
     public final Rect normalize() {
         if (width < 0) {
             x = x + width;
@@ -122,7 +119,7 @@ public class Rect  {
         return this;
     }
 
-    @JsOverlay
+
     public final boolean intersect(Rect box) {
         if (box == null) {
             return false;
@@ -131,7 +128,7 @@ public class Rect  {
                 !(y + height < box.y) && !(box.y + box.height < y);
     }
 
-    @JsOverlay
+
     public final Rect copyFrom(Rect src) {
         this.x = src.x;
         this.y = src.y;
@@ -140,7 +137,7 @@ public class Rect  {
         return this;
     }
 
-    @JsOverlay
+
     public final Rect copyTo(Rect dest) {
         dest.x = this.x;
         dest.y = this.y;
@@ -149,49 +146,49 @@ public class Rect  {
         return this;
     }
 
-    @JsOverlay
+
     public final double getX() {
         return x;
     }
 
-    @JsOverlay
+
     public final void setX(double x) {
         this.x = x;
     }
 
-    @JsOverlay
+
     public final double getY() {
         return y;
     }
 
-    @JsOverlay
+
     public final void setY(double y) {
         this.y = y;
     }
 
-    @JsOverlay
+
     public final double getWidth() {
         return width;
     }
 
-    @JsOverlay
+
     public final Rect setWidth(double width) {
         this.width = width;
         return this;
     }
 
-    @JsOverlay
+
     public final double getHeight() {
         return height;
     }
 
-    @JsOverlay
+
     public final Rect setHeight(double height) {
         this.height = height;
         return this;
     }
 
-    @JsOverlay
+
     public final Rect set(double startX, double startY, double width, double height) {
         this.x = startX;
         this.y = startY;
