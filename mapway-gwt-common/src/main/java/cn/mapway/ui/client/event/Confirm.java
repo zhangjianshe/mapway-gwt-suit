284f4f23bd330b1ce4529fc12a01990c04205f89
zhangjianshe <zhangjianshe@gmail.com>
2024-04-02 10:55:27 +0800

    add confirm interface

---
+++ mapway-gwt-common/src/main/java/cn/mapway/ui/client/event/Confirm.java
@@ -0,0 +1,12 @@
+package cn.mapway.ui.client.event;
+
+import com.google.gwt.core.client.Callback;
+
+/**
+ * confirm user input
+ */
+public abstract class Confirm implements Callback<String, Object> {
+    @Override
+    public void onFailure(Object reason) {
+    }
+}
