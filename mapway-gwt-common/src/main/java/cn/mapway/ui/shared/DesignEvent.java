5c673f9aa6a28b11b9bccb244f46008737d21592
zhangjianshe <zhangjianshe@gmail.com>
2024-07-09 12:16:15 +0800

    add design event system

--- mapway-gwt-common/src/main/java/cn/mapway/ui/shared/DesignEvent.java
+++ mapway-gwt-common/src/main/java/cn/mapway/ui/shared/DesignEvent.java
@@ -9,6 +9,20 @@ public class DesignEvent {
         this.type = type;
     }
 
+    public final static DesignEvent create(DesignEventType type, Object data) {
+        return new DesignEvent(type, data);
+    }
+
+    public final static DesignEvent createLayout(Object data) {
+        return new DesignEvent(DesignEventType.Layout, data);
+    }
+
+    public final static DesignEvent createSelect(Object data) {
+        return new DesignEvent(DesignEventType.Select, data);
+    }
+    public final static DesignEvent createUpdate(Object data) {
+        return new DesignEvent(DesignEventType.Update, data);
+    }
     public Object getData() {
         return data;
     }
--- mapway-gwt-common/src/main/java/cn/mapway/ui/shared/DesignEventType.java
+++ mapway-gwt-common/src/main/java/cn/mapway/ui/shared/DesignEventType.java
@@ -12,4 +12,5 @@ public enum DesignEventType {
     Cancel,
     Menu,
     Mode,
+    Update
 }
