916da1df6dab0036eb120d83ddddf3815917f8d5
zhangjianshe <zhangjianshe@gmail.com>
2024-07-10 15:12:44 +0800

    add design event method

--- mapway-gwt-common/src/main/java/cn/mapway/ui/shared/DesignEvent.java
+++ mapway-gwt-common/src/main/java/cn/mapway/ui/shared/DesignEvent.java
@@ -23,6 +23,43 @@ public class DesignEvent {
     public final static DesignEvent createUpdate(Object data) {
         return new DesignEvent(DesignEventType.Update, data);
     }
+
+    public final static DesignEvent createMove(Object data) {
+        return new DesignEvent(DesignEventType.Move, data);
+    }
+
+    public final static DesignEvent createDelete(Object data) {
+        return new DesignEvent(DesignEventType.Delete, data);
+    }
+
+    public final static DesignEvent createAdd(Object data) {
+        return new DesignEvent(DesignEventType.Add, data);
+    }
+
+    public final static DesignEvent createUp(Object data) {
+        return new DesignEvent(DesignEventType.Up, data);
+    }
+
+    public final static DesignEvent createDown(Object data) {
+        return new DesignEvent(DesignEventType.Down, data);
+    }
+
+    public final static DesignEvent createOk(Object data) {
+        return new DesignEvent(DesignEventType.Ok, data);
+    }
+
+    public final static DesignEvent createCancel(Object data) {
+        return new DesignEvent(DesignEventType.Cancel, data);
+    }
+
+    public final static DesignEvent createMenu(Object data) {
+        return new DesignEvent(DesignEventType.Menu, data);
+    }
+
+    public final static DesignEvent createMode(Object data) {
+        return new DesignEvent(DesignEventType.Mode, data);
+    }
+
     public Object getData() {
         return data;
     }
