59b2f3178fa89bda28efa7958989a0929a668cdf
zhangjianshe <zhangjianshe@gmail.com>
2024-10-23 21:35:04 +0800

    文本输入框 添加输入类型功能

--- mapway-gwt-common/src/main/java/cn/mapway/ui/client/mvc/attribute/editor/uploader/ImageUploadEditorMetaData.java
+++ mapway-gwt-common/src/main/java/cn/mapway/ui/client/mvc/attribute/editor/uploader/ImageUploadEditorMetaData.java
@@ -3,7 +3,7 @@ package cn.mapway.ui.client.mvc.attribute.editor.uploader;
 import cn.mapway.ui.client.mvc.attribute.marker.AbstractEditorMetaData;
 
 /**
- * EditorSelectorEditorMetaData 对应的编辑器实例数据
+ * ImageUploadEditorMetaData 对应的编辑器实例数据
  */
 public abstract class ImageUploadEditorMetaData extends AbstractEditorMetaData {
     public ImageUploadEditorMetaData() {
