9cc36185b40906b9bcf5e2595e07a255919fe9d4
zhangjianshe <zhangjianshe@gmail.com>
2025-03-15 10:29:26 +0800

    AttributeValue add parameterType

--- mapway-gwt-common/src/main/java/cn/mapway/ui/client/mvc/attribute/AttributeValue.java
+++ mapway-gwt-common/src/main/java/cn/mapway/ui/client/mvc/attribute/AttributeValue.java
@@ -14,21 +14,75 @@ import java.io.Serializable;
  *
  * @author zhang
  */
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class AttributeValue implements Serializable, IsSerializable {
     String name;
     String altName;
     String value;
+    @Deprecated
     Integer inputType;
     /**
-     * 新增 编辑器代码
+     * 新增 编辑器代码 代替 inputType,inputType will be removed
      */
     String editCode;
+    /**
+     * @See ParameterTypeEnum
+     */
+    Integer parameterType;
 
     public AttributeValue(String name, String value) {
         this.name = name;
         this.value = value;
+    }
+    public AttributeValue()
+    {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAltName() {
+        return altName;
+    }
+
+    public void setAltName(String altName) {
+        this.altName = altName;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Integer getInputType() {
+        return inputType;
+    }
+
+    public void setInputType(Integer inputType) {
+        this.inputType = inputType;
+    }
+
+    public String getEditCode() {
+        return editCode;
+    }
+
+    public void setEditCode(String editCode) {
+        this.editCode = editCode;
+    }
+
+    public Integer getParameterType() {
+        return parameterType;
+    }
+
+    public void setParameterType(Integer parameterType) {
+        this.parameterType = parameterType;
     }
 }
