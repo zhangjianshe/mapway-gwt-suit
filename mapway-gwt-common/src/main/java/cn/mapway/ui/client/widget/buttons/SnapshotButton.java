fe3f7d9f347e931fc6c8a5ae8760f0572d707908
zhangjianshe <zhangjianshe@gmail.com>
2025-11-08 14:12:07 +0800

    add snapshot button

---
+++ mapway-gwt-common/src/main/java/cn/mapway/ui/client/widget/buttons/SnapshotButton.java
@@ -0,0 +1,16 @@
+package cn.mapway.ui.client.widget.buttons;
+
+
+import cn.mapway.ui.client.fonts.Fonts;
+import cn.mapway.ui.client.widget.FontIcon;
+
+/**
+ * SnapshotButton
+ *
+ * @author zhang
+ */
+public class SnapshotButton extends FontIcon {
+    public SnapshotButton() {
+        setIconUnicode(Fonts.SNAPSHOT);
+    }
+}
