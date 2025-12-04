e629d2c4fcdfc57f02085ccb07019f0ec354d81e
baoshuaizealot <baoshuaizealot@163.com>
2025-12-04 17:19:47 +0800

    CommonEvent 添加  显示光谱曲线(SHOW_SPECTRAL_CURVE) 事件

--- mapway-gwt-common/src/main/java/cn/mapway/ui/shared/CommonEvent.java
+++ mapway-gwt-common/src/main/java/cn/mapway/ui/shared/CommonEvent.java
@@ -3,6 +3,7 @@ package cn.mapway.ui.shared;
 import cn.mapway.ui.client.event.MessageObject;
 import cn.mapway.ui.client.widget.CommandData;
 import com.google.gwt.event.shared.GwtEvent;
+import sun.awt.geom.Curve;
 
 import java.util.List;
 
@@ -147,6 +148,8 @@ public class CommonEvent extends GwtEvent<CommonEventHandler> {
     public final static int LABEL = 133; // 标签
     public final static int CREATE_FOLDER = 134; // 标签
     public final static int NEED_LOGIN = 135; // 需要登录
+
+    public final static int SHOW_SPECTRAL_CURVE = 136; // 显示光谱曲线
 
 
 
@@ -687,6 +690,10 @@ public class CommonEvent extends GwtEvent<CommonEventHandler> {
         return new CommonEvent(CHANGE_CRS, data);
     }
 
+    public static CommonEvent showSpectralCurveEvent(Object data) {
+        return new CommonEvent(SHOW_SPECTRAL_CURVE, data);
+    }
+
     public boolean isObject() {
         return type == OBJECT;
     }
@@ -1184,6 +1191,10 @@ public class CommonEvent extends GwtEvent<CommonEventHandler> {
     }
     public boolean isLabel() {
         return LABEL == this.type;
+    }
+
+    public boolean isShowSpectralCurve() {
+        return SHOW_SPECTRAL_CURVE == this.type;
     }
 
     public static CommonEvent rbacPermissionChangeEvent(Object data) {
