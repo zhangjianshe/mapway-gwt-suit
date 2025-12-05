ca2b8c20d36095d98965fdc3c441615f20c3855a
baoshuaizealot <baoshuaizealot@163.com>
2025-12-05 14:28:37 +0800

    更新fonts

---
+++ mapway-gwt-common/src/main/java/cn/mapway/ui/client/widget/buttons/RbacRoleResourceButton.java
@@ -0,0 +1,15 @@
+package cn.mapway.ui.client.widget.buttons;
+
+
+import cn.mapway.ui.client.fonts.Fonts;
+import cn.mapway.ui.client.widget.FontIcon;
+
+/**
+ * RBAC_ROLE_RESOURCE
+ * RBAC_ROLE_RESOURCE按钮
+ */
+public class RbacRoleResourceButton extends FontIcon {
+    public RbacRoleResourceButton() {
+        setIconUnicode(Fonts.RBAC_ROLE_RESOURCE);
+    }
+}
