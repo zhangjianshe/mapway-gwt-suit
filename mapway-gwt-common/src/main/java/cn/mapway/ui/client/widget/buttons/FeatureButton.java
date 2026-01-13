e0d0011829b2aa3c628a018df694c58c7e83f86a
baoshuaizealot <baoshuaizealot@163.com>
2026-01-13 20:03:20 +0800

    添加切分按钮

---
+++ mapway-gwt-common/src/main/java/cn/mapway/ui/client/widget/buttons/FeatureButton.java
@@ -0,0 +1,13 @@
+package cn.mapway.ui.client.widget.buttons;
+
+import cn.mapway.ui.client.fonts.Fonts;
+import cn.mapway.ui.client.widget.FontIcon;
+
+/**
+ * 切分按钮
+ */
+public class FeatureButton  extends FontIcon {
+    public FeatureButton() {
+        setIconUnicode(Fonts.FEATURE);
+    }
+}
