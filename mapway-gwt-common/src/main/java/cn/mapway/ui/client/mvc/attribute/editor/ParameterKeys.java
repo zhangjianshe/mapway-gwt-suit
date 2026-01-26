0fe86b8129e096cfa5af874fccf4c14eaa79f0c5
zhangjianshe <zhangjianshe@gmail.com>
2026-01-26 14:06:27 +0800

    add editor runtime parameter constant

--- mapway-gwt-common/src/main/java/cn/mapway/ui/client/mvc/attribute/editor/ParameterKeys.java
+++ mapway-gwt-common/src/main/java/cn/mapway/ui/client/mvc/attribute/editor/ParameterKeys.java
@@ -22,4 +22,5 @@ public class ParameterKeys {
     public static final String KEY_PRODUCT_ID="product_id";
     public static final String KEY_CSS_RULES_PROVIDER="css_rules_provider";
     public static final String KEY_BUTTON_TYPE = "button_type";
+    public static final String KEY_CATALOG_USAGE = "catalog_usage";
 }
