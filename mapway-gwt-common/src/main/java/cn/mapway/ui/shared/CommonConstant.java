9aed33d036e575b2262923cb9bde491c4737f055
zhangjianshe <zhangjianshe@gmail.com>
2026-02-03 18:51:47 +0800

    添加通用组建标签

--- mapway-gwt-common/src/main/java/cn/mapway/ui/shared/CommonConstant.java
+++ mapway-gwt-common/src/main/java/cn/mapway/ui/shared/CommonConstant.java
@@ -9,4 +9,12 @@ public class CommonConstant {
 
     //文件选择编辑器
     public static final String EDITOR_FILE_DIR = "FILE_DIR_EDITOR";
+
+    public static final String TAG_ADMIN = "admin"; //需要管理员权限
+    public static final String TAG_PREFERENCE = "preference"; //这是一个PREFERENCE设置模块
+    public static final String TAG_SUBSYSTEM = "subsystem";
+    public static final String TAG_FULLSCREEN = "fullscreen"; //全屏模块
+    public static final String TAG_NOT_HIDE_TOP_BAR = "hide_top_bar"; //与 FULL_SCREEN 配合使用 可以全屏 但是不隐藏最上面的工具栏
+    public static final String TAG_DEV = "dev"; //开发模块 此模块只能在开发环境中使用
+    public static final String TAG_PROPOSE = "propose";//推荐标签
 }
