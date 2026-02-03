fcbaf8b9f16187801357aa6491dd27f372dbae05
zhangjianshe <zhangjianshe@gmail.com>
2026-02-03 19:00:22 +0800

    添加通用组建标签

--- mapway-gwt-common/src/main/java/cn/mapway/ui/shared/CommonConstant.java
+++ mapway-gwt-common/src/main/java/cn/mapway/ui/shared/CommonConstant.java
@@ -17,4 +17,5 @@ public class CommonConstant {
     public static final String TAG_NOT_HIDE_TOP_BAR = "hide_top_bar"; //与 FULL_SCREEN 配合使用 可以全屏 但是不隐藏最上面的工具栏
     public static final String TAG_DEV = "dev"; //开发模块 此模块只能在开发环境中使用
     public static final String TAG_PROPOSE = "propose";//推荐标签
+    public static final String TAG_HIDDEN = "hidden";//隐藏
 }
