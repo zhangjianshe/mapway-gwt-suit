f667ca4b6956ac492cfbad8088997550672bd8ea
zhangjianshe <zhangjianshe@gmail.com>
2026-02-09 11:58:58 +0800

    添加　初始化模块KEY　常量

--- mapway-gwt-common/src/main/java/cn/mapway/ui/shared/CommonConstant.java
+++ mapway-gwt-common/src/main/java/cn/mapway/ui/shared/CommonConstant.java
@@ -3,9 +3,11 @@ package cn.mapway.ui.shared;
 public class CommonConstant {
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String API_TOKEN = "API-TOKEN";
-    public static final String KEY_LOGIN_USER="KEY_LOGIN_USER";
+    public static final String KEY_LOGIN_USER = "KEY_LOGIN_USER";
     //全局总线　消息Topic
     public static final String BUS_EVENT_MESSAGE = "TOPIC_BUS_EVENT_MESSAGE";
+    //初始化模块参数
+    public static final String KEY_INIT_MODULE_CODE = "KEY_INIT_MODULE_CODE";
 
     //文件选择编辑器
     public static final String EDITOR_FILE_DIR = "FILE_DIR_EDITOR";
