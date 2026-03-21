bcbb27b8f50903866f9a4549393e4616aa8de856
zhangjianshe <zhangjianshe@gmail.com>
2026-03-21 21:33:38 +0800

    ADD MEETING ICON

--- mapway-gwt-common/src/main/java/cn/mapway/ui/client/fonts/Fonts.java
+++ mapway-gwt-common/src/main/java/cn/mapway/ui/client/fonts/Fonts.java
@@ -16,7 +16,8 @@ public class Fonts {
         }
     }
 
-    	public final static String INSERT_BEFORE = "e7f0";
+    	public final static String MEETING = "e7f3";
+	public final static String INSERT_BEFORE = "e7f0";
 	public final static String INSERT_AFTER = "e7f1";
 	public final static String INSERT_CHILD = "e7f2";
 	public final static String EXPAND_OUTLINE = "e7e9";
@@ -725,7 +726,8 @@ public class Fonts {
 	static {
 		unicodes=new HashMap<>();
 		//unicodes.put("console", Fonts.CONSOLE);
-			unicodes.put("INSERT_BEFORE", Fonts.INSERT_BEFORE);
+			unicodes.put("MEETING", Fonts.MEETING);
+	unicodes.put("INSERT_BEFORE", Fonts.INSERT_BEFORE);
 	unicodes.put("INSERT_AFTER", Fonts.INSERT_AFTER);
 	unicodes.put("INSERT_CHILD", Fonts.INSERT_CHILD);
 	unicodes.put("EXPAND_OUTLINE", Fonts.EXPAND_OUTLINE);
